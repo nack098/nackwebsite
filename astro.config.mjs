f7ebd0ff7cb7b2005eb59ffcb36737ce7f6272b4
nack098 <77537263+nack098@users.noreply.github.com>
2023-06-06 18:00:36 +0700

    feat(page): add 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,29 @@ import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
+import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nakuya.dev/',
-  integrations: [sitemap(), react(), image(), tailwind()]
+  integrations: [sitemap(), react(), image(), tailwind()],
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold font-poppins',
+          h2: 'text-2xl font-bold font-poppins',
+          h3: 'text-xl font-bold font-poppins',
+          h4: 'text-lg font-bold font-poppins',
+          h5: 'font-bold font-poppins',
+          h6: 'font-bold font-poppins',
+          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-blue-500 decoration-blue-500'
+        }
+      ]
+    ]
+  }
 })
